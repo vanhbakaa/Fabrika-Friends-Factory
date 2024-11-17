6b780104641c76160a6e9e89280d4274ea07632f
Vanhbaka <vanhbaka2k7@gmail.com>
2024-11-17 11:33:36 +0700

    Fix query bug

--- bot/core/query.py
+++ bot/core/query.py
@@ -39,18 +39,19 @@ class Tapper:
         self.query = query
         self.multi_thread = multi_thread
         try:
-            fetch_data = unquote(self.query).split("user=")[1].split("&chat_instance=")[0]
+            fetch_data = unquote(query).split("user=")[1].split("&chat_instance=")[0]
+            json_data = json.loads(fetch_data)
         except:
             try:
-                fetch_data = unquote(self.query).split("user=")[1].split("&auth_date=")[0]
+                fetch_data = unquote(query).split("user=")[1].split("&auth_date=")[0]
+                json_data = json.loads(fetch_data)
             except:
-                logger.warning(f"Invaild query: {query}")
-                sys.exit()
-        try:
-            json_data = json.loads(fetch_data)
-        except:
-            fetch_data = unquote(fetch_data)
-            json_data = json.loads(fetch_data)
+                try:
+                    fetch_data = unquote(unquote(query)).split("user=")[1].split("&auth_date=")[0]
+                    json_data = json.loads(fetch_data)
+                except:
+                    logger.warning(f"Invaild query: {query}")
+                    sys.exit()
         self.session_name = json_data['username']
         self.first_name = ''
         self.last_name = ''
